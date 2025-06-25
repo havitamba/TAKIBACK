22d5b558fabfea54c6dd492aaf7efe9894c46deb
havitamba <elielielia1@gmail.com>
2025-06-25 10:36:01 +0300

    accept env and prod origins

--- app.ts
+++ app.ts
@@ -5,12 +5,23 @@ import cors from "cors";
 import express from "express";
 
 const app = express();
-app.use(
-  cors({
-    origin: "https://takifront-production.up.railway.app",
-    credentials: true,
-  })
-);
+const allowList = [
+  "http://localhost:5173",
+  "https://takifront-production.up.railway.app",
+];
+const corsOptionsDelegate = function (req: any, callback: any) {
+  let corsOptions;
+  if (allowList.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = {
+      origin: req.header("Origin"),
+      credentials: true,
+    }; // reflect the origin of the request
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+app.use(cors(corsOptionsDelegate));
 
 const port = 5000;
 const httpServer = http.createServer(app);
